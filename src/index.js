4e379a34f4beab56085eefbf72edd4c0f527311a
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-02-05 21:13:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert ("test")
+function generatePoem(event) {
+    event.preventDefault()
+
+    alert("Generating Poem")
+}
+
+
+let poemFormElment = document.querySelector("#poem-generator-form");
+poemFormElment.addEventListener("submit", generatePoem);
