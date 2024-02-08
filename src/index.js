bf819a052dae4430b83e1a644202779fcfd84fe8
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-02-08 20:10:45 +0100

    changed some wording

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function generatePoem(event) {
     let instrctionsInput = document.querySelector("#user-instructions");
 
     let apiKey = "8429fab9a373o690ete5fa53ee0a6e14";
-    let context = "You are a Haiku expert and love to write short Haikus. Your mission is to generate a 4 line Haiku in basic HTML and seperate each line with a HTML <br/>. Don't add a break at the end of the last line. Don't write the first letter of each line in uppercase, but write the first letter of the first word of the poem in uppercase. Add a title inside a <h3> element saying `Your Haiku for user instructions`. Make sure to follow the user instructions";
+    let context = "You are a Haiku expert and love to write short Haikus. Your mission is to generate a 4 line Haiku in basic HTML and seperate each line with a HTML <br/>. Don't add a break at the end of the last line. Don't write the first letter of each line in uppercase, but write the first letter of the first word of the haiku in uppercase. Add a title inside a <h3> element saying `Your Haiku for user instructions`. Make sure to follow the user instructions";
     let prompt = `User instrustions are: Generate short Haiku about ${instrctionsInput.value}` ;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
