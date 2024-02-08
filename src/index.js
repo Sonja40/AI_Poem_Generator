7e88a24e87a59dd9b5c8bf3b63719418491408b9
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-02-08 18:15:05 +0100

    added axios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+function displayPoem(response) {
+  new Typewriter('#poem', {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+
 function generatePoem(event) {
     event.preventDefault()
 
@@ -6,21 +16,19 @@ function generatePoem(event) {
     let apiContext = "";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
     
+    console.log()
+
     axios.get(apiURL).then(displayPoem);
 
 
-    new Typewriter('#poem', {
-        strings: "An old silent pond, A frog jumps into the pond. Splash! Silence again." ,
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
+
 
 }
 
-
+ 
 let poemFormElment = document.querySelector("#poem-generator-form");
 poemFormElment.addEventListener("submit", generatePoem);
 
-api Key = "8429fab9a373o690ete5fa53ee0a6e14"
-api 
+
+
+"An old silent pond, A frog jumps into the pond. Splash! Silence again." 
