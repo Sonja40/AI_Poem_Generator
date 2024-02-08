52dbbb590492765fc5f26e7ca988eb93db42120e
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-02-08 18:14:50 +0100

    created API function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 function generatePoem(event) {
     event.preventDefault()
 
+    let apiKey = "8429fab9a373o690ete5fa53ee0a6e14";
+    let apiPrompt = "Tell me a short nice Poem";
+    let apiContext = "";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+    
+    axios.get(apiURL).then(displayPoem);
+
+
     new Typewriter('#poem', {
         strings: "An old silent pond, A frog jumps into the pond. Splash! Silence again." ,
         autoStart: true,
@@ -14,3 +22,5 @@ function generatePoem(event) {
 let poemFormElment = document.querySelector("#poem-generator-form");
 poemFormElment.addEventListener("submit", generatePoem);
 
+api Key = "8429fab9a373o690ete5fa53ee0a6e14"
+api 
